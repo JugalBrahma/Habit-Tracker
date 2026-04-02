83af68d42f863ab7cabcd6a3a8980c492d680f6c
JugalBrahma <jryzengame@gmail.com>
2026-04-02 20:33:49 +0530

    '

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -48,8 +48,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.0.1"
+        versionCode = 4
+        versionName = "1.0.3"
         multiDexEnabled = true
     }
 
